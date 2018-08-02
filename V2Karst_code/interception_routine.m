5136068df41da048ef1f8f7d9aa9ef477003fa8f
fannysarrazin <34069847+fannysarrazin@users.noreply.github.com>
2018-08-02 15:52:53 +0100

    Update interception_routine.m
    - The time conversion factor Kt has been included as the input of the function to allow for daily and subdaily applications.
    - The model includes a state variable (interception storage to allow for sub-daily applications).

--- V2Karst_code/interception_routine.m
+++ V2Karst_code/interception_routine.m
@@ -1,13 +1,15 @@
-function [Tf,t_wet,Ecan_act] = interception_routine(Vcan,LAI,fc,P,Ecan_pot)
+function [Tf,t_wet,Ecan_act] = interception_routine(Vcan,LAI,fc,P,Ecan_pot,Kt)
 
-% The function estimates daily actual evaporation from canopy interception.
-% The formulation is similar to the daily threshold model proposed by 
-% (Savenije, 1997; de Groen, 2002; Gerrits, 2010) and do not consider the
-% carry-over of interception from one day to the next (all precipitation 
-% which is not evaporated reaches the ground as throughfall).
+% The function estimates actual evaporation from canopy interception.
+% For daily time step, the formulation is similar to the daily threshold 
+% model proposed by (Savenije, 1997; de Groen, 2002; Gerrits, 2010) and do 
+% not consider the carry-over of interception from one day to the next 
+% (all precipitation which is not evaporated reaches the ground as 
+% throughfall). The carry-over of the interception storage is considered
+% for sub-daily time steps.
 %
 % USAGE:
-% [Tf,t_wet,Ecan_act] = interception_routine(Vcan,LAI,fc,P,Ecan_pot)
+% [Tf,t_wet,Ecan_act] = interception_routine(Vcan,LAI,fc,P,Ecan_pot,Kt)
 %
 % INPUTS
 % PARAMETERS:
@@ -32,11 +34,13 @@ function [Tf,t_wet,Ecan_act] = interception_routine(Vcan,LAI,fc,P,Ecan_pot)
 %             in the funciton soil_epikarst_routine.m)
 % Ecan_act = daily actual evaporation from canopy
 %             interception [mm]                               - vector(H,1)
+% Kt = time conversion factor [s/time step]                        - scalar 
+%      At daily time step 3600*24 s/d
 %
 % REFERENCES:
 % Bohn, T. J., and E. R. Vivoni (2016), Process-based characterization of 
 % evapotranspiration sources over the North American monsoon region, Water 
-% Resour. Res., 52(1), 358–384, doi:10.1002/2015WR017934.
+% Resour. Res., 52(1), 358â€“384, doi:10.1002/2015WR017934.
 %
 % De Groen, M. M.(2002), Modelling interception and transpiration at monthly 
 % time steps: introducing daily variability through Markov chains, Ph.D. 
@@ -46,23 +50,23 @@ function [Tf,t_wet,Ecan_act] = interception_routine(Vcan,LAI,fc,P,Ecan_pot)
 % thesis, Delft University of Technology, Delft, The Netherlands.
 % 
 % Kergoat, L. (1998), A model for hydrological equilibrium of leaf area 
-% index on a global scale, J. Hydrol.,212–213, 268–286, 
+% index on a global scale, J. Hydrol.,212â€“213, 268â€“286, 
 % doi:10.1016/S0022-1694(98)00211-X.
 %
-% Sarrazin, F., A. Hartmann, F. Pianosi, and T. Wagener (2017), V2Karst: 
+% Sarrazin, F., A. Hartmann, F. Pianosi, and T. Wagener (2018), V2Karst: 
 % A parsimonious large-scale integrated vegetation-recharge model to  
 % simulate the impact of climate and land cover change in karst regions. 
 % Geosci. Model Dev. In review.
 %
 % Savenije, H. H. G. (1997), Determination of evaporation from a catchment 
 % water balance at a monthly time scale, Hydrol. Earth Syst. Sci., 1(1), 
-% 93–100, doi:10.5194/hess-1-93-1997.
+% 93â€“100, doi:10.5194/hess-1-93-1997.
 
-% This function is part the V2Karst model (Sarrazin et al., 2017). 
+% This function is part the V2Karst model (Sarrazin et al., 2018). 
 % V2Karst is provided under the terms of the GNU General Public License 
 % version 3.0.
 % This function was prepared by Fanny Sarrazin, University of Bristol,
-% December 2017 (fanny.sarrazin@bristol.ac.uk).
+% August 2018 (fanny.sarrazin@bristol.ac.uk).
 
 %--------------------------------------------------------------------------
 % 1. Prepare variables
@@ -78,46 +82,72 @@ if isscalar(fc);fc = fc*ones(H,1);end
 Ecan_act = nan(H,1); % Actual evaporation from interception [mm]
 Tf = nan(H,1); % Throughfall [mm]
 t_wet = nan(H,1); % Fraction of day with wet canopy [-]
+Ic       = zeros(H,1); % Interception storage over the vegetated fraction
 
 %--------------------------------------------------------------------------
 % 3. Interception routine
 %--------------------------------------------------------------------------
 
 for t=1:H
+    if t==1
+         Ic_0=0; 
+     % if no leaves in the canopy: no interception    
+     elseif LAI(t)==0
+         Ic_0=0;
+         Tf(t-1)=Tf(t-1)+Ic(t-1); % if the plant is loosing all 
+         % its leaves between step t-1 and t, the intercepted water remaining 
+         % in the canopy is directed to the ground.
+     else
+         Ic_0=Ic(t-1);
+    end
+     
     % Effective canopy storage capacity over the vegetated fraction
     %(cell average LAI is rescaled to the vegetated fraction following 
     % Bohn and Vivoni, 2016)
     Vcan_max = Vcan*(LAI(t)./fc(t)); % [m2/m2] over vegetated fraction
     
     % Evaporation from interception store is limited by the potential rate
-    % and storage capacity
+    % and storage capacity (value over vegetated fraction)
     Ecan_act_dummy = min(Ecan_pot(t),Vcan_max,'includenan');
     
-    % Correction (Ecan_act cannot be higher than interception precipitation) 
+    % Correction (Ecan_act cannot be higher than interception precipitation)
     % and area-weighting by fc since interception occurs over the vegetated
-    % fraction only
-    Ecan_act(t) = fc(t).*min(Ecan_act_dummy,P(t),'includenan');
+    % fraction only (value for the cell)
+    Ecan_act(t) = fc(t).*min(Ecan_act_dummy,P(t)+Ic_0,'includenan');
+    
+    % Update canopy storage (over vegetated fraction)
+    Ic(t)=min(Ic_0+P(t)-Ecan_act(t)/fc(t),Vcan_max,'includenan');
+    
+    if Kt==86400 % no carry-over of interception storage for daily time step
+        Ic(t) = 0;
+    end
     
     % Throughfall
-    Tf(t) = P(t)-Ecan_act(t);
-  
-    % Assess the fraction of day with wet canopy 
+    Tf(t) = max(P(t)-Ecan_act(t)-(Ic(t)-Ic_0)*fc(t),0);
+    
+    % Code check
+    if round(Tf(t)*10^8)>round(P(t)*(1-fc(t))*10^8) && Ic(t)<Vcan_max && Kt<86400
+        error('throughfall with unsaturated canopy')
+    end
+
+    % Assess the fraction of time step with wet canopy 
     if Ecan_pot(t)==0 
         t_wet(t) = 0; % the canopy is assumed dry during the entire day
     else
         t_wet(t) = Ecan_act(t)/(Ecan_pot(t).*fc(t)); % similar to Kergoat (1998)
     end
 end
-
 %--------------------------------------------------------------------------
 % 4. Check variables
 %--------------------------------------------------------------------------
 if any(isnan(t_wet));error('t_wet contains NaNs');end
 if any(isnan(Tf));error('Tf contain NaNs');end
 if any(isnan(Ecan_act));error('Ecan_act contains NaNs');end
+if any(isnan(Ic));error('Ic contains NaNs');end
 
 if any(t_wet>1 | t_wet<0);error('values of t_wet are not between 0 and 1');end
 if any(Tf<0);error('Tf contains negative values');end
 if any(Ecan_act<0);error('Ecan_act contains negative values');end
+if any(isnan(Ic));error('Ic contains contains negative values');end
 if any(Tf>P);error('Tf contains values higher than P');end
 
