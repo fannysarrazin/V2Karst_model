a4990015e361cda02d7abe2e3ff2819cb07875e7
fannysarrazin <34069847+fannysarrazin@users.noreply.github.com>
2018-08-02 15:37:58 +0100

    Update Penman_Monteith.m
    - The time conversion factor Kt has been included as the input of the function to allow for daily and subdaily applications.
    - The handling of temperature and humidity input data (mean, min and max values) has been revised.

--- V2Karst_code/Penman_Monteith.m
+++ V2Karst_code/Penman_Monteith.m
@@ -1,28 +1,30 @@
-function E_pot = Penman_Monteith(rs,ra,Rn,Ta,RH,G,Pa)
+function E_pot = Penman_Monteith(rs,ra,Rn,Ta,RH,G,Pa,Kt)
 
-% This function calculates the daily potential evapotranspiration rate 
-% using the Penman Monteith equation (Monteith, 1965). 
-% INPORTANT NOTE:
-% For subdaily calculation the time convertion factor 'Kt' defined in the 
-% section 2 of the function would have to be changed.
+% This function calculates potential evapotranspiration rate 
+% using the Penman Monteith equation (Monteith, 1965) for daily or subdaily
+% time step.
 %
 % USAGE: 
-% E_pot = Penman_Monteith(rs,ra,Rn,Ta,RH,G,Pa)
+% E_pot = Penman_Monteith(rs,ra,Rn,Ta,RH,G,Pa,Kt)
 %
 % INPUTS: 
 % rs = surface resistance [s/m]                     - scalar or vector(H,1)
 % ra = aerodynamic resistance [s/m]                 - scalar or vector(H,1)
 % Rn = daily net radiation [MJ/m2/d]                          - vector(H,1)
-% Ta = mean daily air temperature                             - vector(H,1)
-%      or matrix of minimum (Ta(:,1)) and                    or vector(H,2)    
-%      maximum (Ta(:,2)) daily air temperature [°C]   
+% Ta = mean air temperature                                   - vector(H,1)
+%      or matrix of mean (Ta(:,1)),                          or vector(H,3)    
+%      minimum(Ta(:,2))and maximum (Ta(:,3)temperature [Â°C]   
 %     (see Note below)
-% RH = mean daily relative humidity                           - vector(H,1)
-%      or matrix of minimum (RH(:,1))and                     or vector(H,2) 
-%      maximum(RH(:,2)) daily relative humidity [%]   
+% RH = mean relative humidity                                 - vector(H,1)
+%      or matrix of mean (RH(:,1)),                          or vector(H,3) 
+%      minimum (RH(:,2))and maximum(RH(:,3)) 
+%      relative humidity [%]   
 %      (see Note below)
 %  G = daily ground heat flux [MJ/m2/d]                       - vector(H,1)
 % Pa = atmospheric pressure [kPa]                   - scalar or vector(H,1)
+% Kt = conversion factor to estimate aerodynamic                    -scalar 
+%      component (Allen et al., 1998 p.26) [s/step]
+%      At daily time step 3600*24 s/d
 %
 % OUTPUTS:
 % E_pot = daily potential evapotranspiration [mm]               - vect(H,1)
@@ -41,7 +43,7 @@ function E_pot = Penman_Monteith(rs,ra,Rn,Ta,RH,G,Pa)
 % Paper 56, Food and Agriculture Organization (FAO), Rome, Italy.
 %
 % Monteith, J. L.(1965), Evaporation and environment, Symp. Soc. Exp. Biol., 
-% 19, 205–234.
+% 19, 205â€“234.
 %
 % Sarrazin, F., A. Hartmann, F. Pianosi, and T. Wagener (2017), V2Karst: 
 % A parsimonious large-scale integrated vegetation-recharge model to  
@@ -58,27 +60,21 @@ function E_pot = Penman_Monteith(rs,ra,Rn,Ta,RH,G,Pa)
 % V2Karst is provided under the terms of the GNU General Public License 
 % version 3.0.
 % This function was prepared by Fanny Sarrazin, University of Bristol,
-% December 2017 (fanny.sarrazin@bristol.ac.uk).
+% August 2018 (fanny.sarrazin@bristol.ac.uk).
 
 %--------------------------------------------------------------------------
 % 1. Prepare variables 
 %--------------------------------------------------------------------------
 nb_Ta=size(Ta,2); % number of colums in temperature input
 nb_RH=size(RH,2); % number of colums in relative humidity input
-Ta_m=mean(Ta,2); % mean daily temperature [°C] 
-RH_m=mean(RH,2); % mean relative humidity [%]
 
 %--------------------------------------------------------------------------
 % 2. Define constant parameters
 %--------------------------------------------------------------------------
 cp = 1.013*10^(-3); % specific heat of air at constant pressure for moist  
-                    % air [MJ/kg/°C]
+                    % air [MJ/kg/Â°C]
 epsilon = 0.622; % ratio molecular weight of water vapour/dry air [-]
 R = 0.287; % specific gas constant [kJ/kg/K]
-Kt = 86400; % convertion factor to estimate aerodynamic component at 
-            % daily time step (Allen et al., 1998 p.26) [s/d]
-            % (number of second per day: 3600*24 s/d)
-            % The value of Kt has to be changed for subdaily calculations
 
 %--------------------------------------------------------------------------
 % 3. Assess variables used in Penman Monteith equation
@@ -87,26 +83,26 @@ Kt = 86400; % convertion factor to estimate aerodynamic component at
 %--------------------------------------------------------------------------
 % 3.1 Latent heat of vaporization
 %--------------------------------------------------------------------------
-% Formula used in Sarrazin et al., (2017) (Shuttleworth, 1993, Eq.(4.2.1))
-lambda = 2.501-0.002361*Ta_m;% [MJ/kg] 
-% Other option: simplication for a temperature of 20°C (Allen et al., 1998, p.3)
+% Formula used in Sarrazin et al., (2018) (Shuttleworth, 1993, Eq.(4.2.1))
+lambda = 2.501-0.002361*Ta(:,1);% [MJ/kg] 
+% Other option: simplication for a temperature of 20Â°C (Allen et al., 1998, p.3)
 % lambda=2.45; % [MJ/kg] 
 
 %--------------------------------------------------------------------------
 % 3.2 Psychrometric constant (Shuttleworth, 1993, Eq.(4.2.28); 
 %     Allen et al, 1998, Eq.(8))
 %--------------------------------------------------------------------------
-gamma = cp*Pa./(lambda*epsilon); % [kPa/°C] 
+gamma = cp*Pa./(lambda*epsilon); % [kPa/Â°C] 
 
 %--------------------------------------------------------------------------
 % 3.3 Saturation vapour pressure (Shuttleworth, 1993, Eq.(4.2.2); 
 %     Allen et al., 1998, Eq. (11-12))
 %--------------------------------------------------------------------------
 if nb_Ta==1
-    es = 0.6108*exp((17.27*Ta)./(Ta+237.3));% [kPa] 
-elseif nb_Ta==2
-    es_min = 0.6108*exp((17.27*Ta(:,1))./(Ta(:,1)+237.3)); % [kPa]
-    es_max = 0.6108*exp((17.27*Ta(:,2))./(Ta(:,2)+237.3)); % [kPa]
+    es = 0.6108*exp((17.27*Ta(:,1))./(Ta(:,1)+237.3));% [kPa] 
+elseif nb_Ta==3
+    es_min = 0.6108*exp((17.27*Ta(:,2))./(Ta(:,2)+237.3)); % [kPa]
+    es_max = 0.6108*exp((17.27*Ta(:,3))./(Ta(:,3)+237.3)); % [kPa]
     es = (es_min+es_max)/2; % [kPa]
 end
 
@@ -114,22 +110,22 @@ end
 % 3.4 Actual vapor pressure (Allen et al., 1998, Eq.(17-19))
 %--------------------------------------------------------------------------
 if nb_RH==1 || nb_Ta==1
-    ea = RH_m.*es/100; % [kPa]
-elseif nb_RH==2 && nb_Ta==2 % [kPa]
-    ea = (RH(:,1).*es_max+RH(:,2).*es_min)/100/2;
+    ea = RH(:,1).*es/100; % [kPa]
+elseif nb_RH==3 && nb_Ta==3 % [kPa]
+    ea = (RH(:,2).*es_max+RH(:,3).*es_min)/100/2;
 end
 
 %--------------------------------------------------------------------------
 % 3.5 Gradient of the saturated vapour pressure-temperature function 
 % (Shuttleworth, 1993, Eq.(4.2.3); Allen et al., 1998, Eq.(13))
 %--------------------------------------------------------------------------
-delta = 4098*0.6108*exp((17.27*Ta_m)./(Ta_m+237.3))./((Ta_m+237.3).^2);
-% [kPa/°C] 
+delta = 4098*0.6108*exp((17.27*Ta(:,1))./(Ta(:,1)+237.3))./((Ta(:,1)+237.3).^2); 
+% [kPa/Â°C] 
 
 %--------------------------------------------------------------------------
 % 3.6 Atmospheric density (Allen et al., 1998, Eq.(3.6))
 %--------------------------------------------------------------------------
-TK = (Ta_m+273.15);% temperature [K]
+TK = (Ta(:,1)+273.15);% temperature [K], uses mean air temperature
 TKv = TK./(1-0.378*ea./Pa); % Virtual temperature [K]
 ro_a = Pa./(R*TKv); % atmospheric density [kg/m3]
 
